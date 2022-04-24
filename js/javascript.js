eb15be9bede08384b1e63016e7433582a0830ad3
Volha Kaskevich <volha.kaskevich@emu.ee>
2022-04-24 20:42:51 +0300

    Districts added

--- js/javascript.js
+++ js/javascript.js
@@ -1,30 +1,45 @@
 let map = L.map('map').setView([58.373523, 26.716045], 12)
 
 const osm =
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-maxZoom: 19,
-attribution: 'OpenStreetMap contributors',
-})
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: 'OpenStreetMap contributors',
+    })
 
 osm.addTo(map)
 
 async function addDistrictsGeoJson(url) {
-const response = await fetch(url)
-const data = await response.json()
-const polygons = L.geoJson(data)
-polygons.addTo(map)
+    // const response = await fetch(url)
+    // const data = await response.json()
+    // const polygons = L.geoJson(data)
+    // console.log(polygons)
+    // polygons.addTo(map)
+    fetch(url)
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            polygons.addTo(map)
+        });
 }
 
-addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
+// addDistrictsGeoJson('./geojson/tartu_city_districts_edu.geojson')
 
-osm.addTo(map)
+// osm.addTo(map)
 
 async function addCelltower(url) {
-const response = await fetch(url)
-const data = await response.json()
-const polygons = L.geoJson(data)
-polygons.addTo(map)
+    const response = await fetch(url)
+    const data = await response.json()
+    const polygons = L.geoJson(data)
+    polygons.addTo(map)
 
-addCelltowers('tartu_city_celltowers_edu.geojson')
-var markers = L.markerClusterGroup('tartu_city_celltowers_edu.geojson');
-markers.addLayer(L.marker(getRandomLatLng(map)));
+    addCelltowers('tartu_city_celltowers_edu.geojson')
+    var markers = L.markerClusterGroup('tartu_city_celltowers_edu.geojson');
+    markers.addLayer(L.marker(getRandomLatLng(map)));
+}
+
+fetch('geojson/tartu_city_districts_edu.geojson')
+    .then(response => response.json())
+    .then(json => {
+        console.log(json);
+        polygons.addTo(map)
+    });
